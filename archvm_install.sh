593a6af00a1030b86b105618e3979867391f3355
Marc <67747758+marc-cenon@users.noreply.github.com>
2022-05-14 23:02:02 +0200

    Update archvm_install.sh

--- archvm_install.sh
+++ archvm_install.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
-### export package list as variable
-package_list="$(cat package_list.txt)"
-sudo pacman -Syyuu
+### prepare package_list.txt to be used by paru
+package_list=$(awk '{print $1}'  package_list.txt  | sed '/^#/d' | tr '\n' ' ')
 
+# update system and make sure basic packages are installed
+sudo pacman -Syyuu --noconfirm
+sudo pacman -S --needed base-devel --noconfirm
+
+# install packages for vm environment
 echo "install vm needed packages"
 sudo pacman -S xf86-input-vmmouse xf86-video-vmware mesa open-vm-tools
 sudo systemctl enable vmtoolsd.service
 sudo systemctl restart vmtoolsd.service
-
-#### ARCH BASIC INSTALL ####
-sudo pacman -S --needed base-devel
 
 #### PARU INSTALL ####
 echo "installing paru"
@@ -18,7 +19,19 @@ git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si
 
 ### PACKAGES ####
 echo "Installing packages from list"
-paru -S $(awk '{print $1}'  package_list.txt  | sed '/^#/d' | tr '\n' ' ')
+paru -S $package_list --noconfirm
+
+### OH MY ZSH
+echo "installing oh my zsh"
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+#### FZF INSTALL ####
+echo "installing FUZZ"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+### copying ZSH conf
+cp .zshrc $HOME
 
 #### APPLYING TMUX AND VIM CONFIG
 echo "copying vimrc and tmux in $HOME "
@@ -30,14 +43,4 @@ done
 echo "copying .confing folder"
 cp -r config $HOME/.config/
 
-### OH MY ZSH
-echo "installing oh my zsh"
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
-### copying ZSH conf
-cp .zshrc $HOME
-
-#### FZF INSTALL ####
-echo "installing FUZZ"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
