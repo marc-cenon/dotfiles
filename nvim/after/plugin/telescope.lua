82c412f9f4bc822fc3f93718609addf8b87a198d
Marc Cenon <marc.cenon33@gmail.com>
2023-07-25 11:27:35 +0200

    update

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,9 +1,6 @@
 local status, telescope = pcall(require, "telescope")
-local status2, actions = pcall(require, "telescope.actions")
 
-if (not status and status2) then return end
-
-local fb_actions = require "telescope".extensions.file_browser.actions
+if (not status) then return end
 
 telescope.load_extension("file_browser")
 telescope.load_extension('dap')
@@ -14,20 +11,10 @@ telescope.setup {
     layout_config = {
       prompt_position = 'top',
     },
-    mappings = {
-      i = {
-        ["<C-w>"] = function() vim.cmd('normal vbd') end,
-        ["<esc>"] = actions.close,
-        ["<C-a>"] = actions.toggle_selection,
-        ["<Tab>"] = actions.move_selection_next,
-        ["<S-Tab>"] = actions.move_selection_previous,
-      },
-    },
   },
 }
 
 -- load extensions
-require("telescope").load_extension "file_browser"
-require("telescope").load_extension "emoji"
+require("telescope").load_extension('file_browser')
+require("telescope").load_extension('emoji')
 require("telescope").load_extension('harpoon')
-
